1e655768f3473205fc129e4ec6f1944d3af81201
Grefeer <815733200@qq.com>
2025-12-28 17:35:21 +0800

    用户管理模块接口+文档上传下载模块+langchain4j+rabbitmq+redis version: 0.75

--- src/main/java/com/aiqa/project1/worker/AnswerWorker.java
+++ src/main/java/com/aiqa/project1/worker/AnswerWorker.java
@@ -25,17 +25,12 @@ public class AnswerWorker {
 
     private final OpenAiChatModel douBaoLite;
     private static final String ANSWER_TEMPLATE = """
-                你是多智能体系统的问答节点，核心任务是结合历史对话上下文与检索信息，准确全面回答用户当前问题。
+                你是多智能体系统的问答节点，核心任务是结合历史对话上下文，准确全面回答用户当前问题。
                 
                 首先，阅读以下历史对话，理解背景及用户此前交互：
                 <历史对话>
                 %s
                 </历史对话>
-                
-                接下来，查看回答当前问题的检索信息：
-                <检索信息>
-                %s
-                </检索信息>
                 
                 当前用户问题：
                 <用户查询>
@@ -43,43 +38,42 @@ public class AnswerWorker {
                 </用户查询>
                 
                 回答规则：
-                1. 结合历史上下文逻辑，确保回答与之前交互连贯一致，回答简洁明了，保证信息完整准确
-                2. 回答内容需基于检索信息，禁止编造未提及内容，若无直接相关检索信息，明确告知无法回答
+                1. 结合历史上下文逻辑，确保回答与之前交互连贯一致，回答完整有逻辑，保证信息完整准确
+                2. 回答内容需基于历史信息，禁止编造未提及内容，若无直接相关信息，明确告知无法回答
                 3. 问题和回答的语言要一致，这是十分重要的
                 4. 如果用户要求“介绍论文”，回答必须包含论文的核心问题、提出的方法、关键创新点和主要实验结果
                 现在，请开始回答。
                 """;
     private final RabbitTemplate rabbitTemplate;
     private final RedisStoreUtils redisStoreUtils;
+    private final CacheAsideUtils cacheAsideUtils;
 
-    public AnswerWorker(OpenAiChatModel douBaoLite, RabbitTemplate rabbitTemplate, RedisStoreUtils redisStoreUtils) {
+    public AnswerWorker(OpenAiChatModel douBaoLite, RabbitTemplate rabbitTemplate, RedisStoreUtils redisStoreUtils, CacheAsideUtils cacheAsideUtils) {
         this.douBaoLite = douBaoLite;
         this.rabbitTemplate = rabbitTemplate;
         this.redisStoreUtils = redisStoreUtils;
+        this.cacheAsideUtils = cacheAsideUtils;
     }
 
     @RabbitListener(bindings = @QueueBinding(
             value = @Queue(value = "answerWorker.queue",durable = "true"),
-            exchange = @Exchange(name = "answer.topic", type = ExchangeTypes.TOPIC),
-            key = "have.gathered.retrieve"
+            exchange = @Exchange(name = SystemConfig.REFLECTION_DIRECT, type = ExchangeTypes.DIRECT),
+            key = SystemConfig.NO_PROBLEM_KEY
     ))
     public void run(State state) {
-        String chatHistory = redisStoreUtils.getChatMemory(state.getUserId(), state.getSessionId(), 10).stream().map(Object::toString).collect(Collectors.joining("\n"));
+        String chatHistory = String.join("\n", cacheAsideUtils.getChatMemory(state.getUserId(), state.getSessionId()));
 
-        String retrievalInfoText = redisStoreUtils.getRetrievalInfo(state.getUserId(), state.getSessionId(),state.getMemoryId(), "retrieve")
-                .stream()
-                .map(Object::toString)
-                .distinct()
-                .collect(Collectors.joining("\n"));
+        System.out.println("AnswerWorker:" + state);
 
-        String query = state.getQuery();
+        String prompt = ANSWER_TEMPLATE.formatted(
+                chatHistory,
+                state.getQuery());
 
-        String prompt = ANSWER_TEMPLATE.formatted(chatHistory, retrievalInfoText, query);
         String answer = douBaoLite.chat(prompt);
         Long idx = redisStoreUtils.setChatMemory(state.getUserId(), state.getSessionId(), "<AI思考>" + answer);
         // 延迟，等到redis保存及以后再向rabbitmq发送数据
         if (idx != null && idx > 0) {
-            rabbitTemplate.convertAndSend("reflection", state);
+            rabbitTemplate.convertAndSend("result", state);
         }
     }
 }
